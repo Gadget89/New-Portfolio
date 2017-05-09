54548542681266bdbbcb2784610889e1096648b8
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-05-09 11:45:48 -0500

    Added a 404 Route to handle pages not found

---
+++ js/ErrorPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router'
+var createReactClass = require("create-react-class")
+
+export default createReactClass({
+
+  render() {
+    return (
+      <section>
+        <h1 className='test'>Error 404: Page not found!</h1>
+      </section>
+    )
+  }
+})
--- js/index.js
+++ js/index.js
@@ -3,12 +3,14 @@ import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 import App from './App'
 import Welcome from './Welcome'
+import ErrorPage from './ErrorPage'
 
 render(
   (
     <Router history={hashHistory}>
       <Route component={ App }>
         <Route path="/" component={ Welcome } />
+        <Route path="*" component={ ErrorPage } />
       </Route>
     </Router>
   ),
