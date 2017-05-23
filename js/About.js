bd1ccccced863e5024269bd7744e406cf2640dd5
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-05-23 13:25:18 -0500

    Added a title and sub-title to landing area

--- js/About.js
+++ js/About.js
@@ -7,19 +7,36 @@ export default createReactClass({
 
   render() {
     return (
-      <article>
-        <article className="landing"></article>
-          <img className="hexBack" src="/styles/grid.png"></img>
-            <section className="hexagonPhotoWrapper">
-              <div className="hexagonPhoto">
-                <div className="hexTopPhoto"></div>
-                <div className="hexBottomPhoto"></div>
-              </div>
-            </section>
-
-      </article>
-
-
+      <section>
+        <section
+          className="landing">
+        </section>
+        <img
+          className="hexBack"
+          src="/styles/grid.png">
+        </img>
+        <h1
+          className="landingName">
+          nicolas roybal
+        </h1>
+        <h2 className="landingTitle">
+          front end developer
+        </h2>
+        <section
+          alt="Photo of Nicolas Roybal"
+          className="hexagonPhotoWrapper"
+          title="Nicolas Roybal">
+          <div
+            className="hexagonPhoto">
+            <div
+              className="hexTopPhoto">
+            </div>
+            <div
+              className="hexBottomPhoto">
+            </div>
+          </div>
+        </section>
+      </section>
     )
   }
 })
--- js/Loader.js
+++ js/Loader.js
@@ -4,7 +4,7 @@ var createReactClass = require("create-react-class")
 
 
 const runLoader = () => {
-  setTimeout(function(){ document.body.className+="loaded"; }, 1000);
+  setTimeout(function(){ document.body.className+="loaded"; }, 3000);
 }
 window.onload = runLoader;
 
