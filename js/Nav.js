7e7651d9a945b00ddcc34e2c8630c11b5582ffcf
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-05-23 23:04:12 -0500

    Changed header content to upper case and corrected rendering issues

--- js/Nav.js
+++ js/Nav.js
@@ -18,41 +18,46 @@ export default createReactClass({
             <li>
               <a
                 className="nav_button"
-                href="#"
+                href="https://drive.google.com/file/d/0B52FYDuPpv0fdjAzcnJSSUttMFU/view?usp=sharing"
+                target="_blank"
                 title="Résumé">
-                Résumé
+                résumé
               </a>
             </li>
             <li>
               <a
                 className="nav_button"
-                href="#"
-                title="Skills">
-                Skills
-              </a>
-            </li>
-            <li>
-              <a
-                className="nav_button"
-                href="#"
-                title="Portfolio">
-                Portfolio
-              </a>
-            </li>
-            <li>
-              <a
-                className="nav_button"
-                href="#"
+                href="https://form.jotform.us/70884700284156"
+                target="_blank"
                 title="Contact">
-                Contact
+                contact
               </a>
             </li>
             <li>
               <a
                 className="nav_button"
                 href="#"
+                id="disabled_element"
+                title="Skills">
+                skills
+              </a>
+            </li>
+            <li>
+              <a
+                className="nav_button"
+                href="#"
+                id="disabled_element"
+                title="Portfolio">
+                portfolio
+              </a>
+            </li>
+            <li>
+              <a
+                className="nav_button"
+                href="#"
+                id="disabled_element"
                 title="About">
-                About
+                about
               </a>
             </li>
           </ul>
