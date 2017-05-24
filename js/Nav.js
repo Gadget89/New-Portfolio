0eecc13a98bbdb56c9bd209ae98a6d08b41aa14d
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-05-23 20:48:36 -0500

    Created static nav backdrop and styled nav bar elements

--- js/Nav.js
+++ js/Nav.js
@@ -7,50 +7,57 @@ export default createReactClass({
 
   render() {
     return (
-      <header className="nav_wrapper">
-        <ul>
-          <li>
-            <a
-              className="nav_button"
-              href="#"
-              title="About">
-              About
-            </a>
-          </li>
-          <li>
-            <a
-              className="nav_button"
-              href="#"
-              title="Portfolio">
-              Portfolio
-            </a>
-          </li>
-          <li>
-            <a
-              className="nav_button"
-              href="#"
-              title="Skills">
-              Skills
-            </a>
-          </li>
-          <li>
-            <a
-              className="nav_button"
-              href="#"
-              title="Contact">
-              Contact
-            </a>
-          </li>
-          <li>
-            <a
-              className="nav_button"
-              href="#"
-              title="Résumé">
-              Résumé
-            </a>
-          </li>
-        </ul>
-      </header>
+      <section>
+        <header className="nav_wrapper_background">
+        </header>
+        <h3 className="nav_name">
+          nicolas roybal
+        </h3>
+        <header className="nav_wrapper">
+          <ul>
+            <li>
+              <a
+                className="nav_button"
+                href="#"
+                title="Résumé">
+                Résumé
+              </a>
+            </li>
+            <li>
+              <a
+                className="nav_button"
+                href="#"
+                title="Skills">
+                Skills
+              </a>
+            </li>
+            <li>
+              <a
+                className="nav_button"
+                href="#"
+                title="Portfolio">
+                Portfolio
+              </a>
+            </li>
+            <li>
+              <a
+                className="nav_button"
+                href="#"
+                title="Contact">
+                Contact
+              </a>
+            </li>
+            <li>
+              <a
+                className="nav_button"
+                href="#"
+                title="About">
+                About
+              </a>
+            </li>
+          </ul>
+        </header>
+      </section>
     )
   }
 })
