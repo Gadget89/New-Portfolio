5d3d4a8c75de077d8ebd95706eabbef4d95d03e7
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-05-26 14:21:45 -0500

    Added content to Portfolio.js and began styling for carousel images

--- js/Portfolio.js
+++ js/Portfolio.js
@@ -9,7 +9,12 @@ export default createReactClass({
       <section>
         <h1 className="heading_txt">portfolio</h1>
         <article className="portfolio_wrapper">
-          
+          <img
+            className="backdrop" src="/styles/me.jpg">
+          </img>
+          <img
+            className="front" src="/styles/new_hex7.png">
+          </img>
         </article>
       </section>
     )
