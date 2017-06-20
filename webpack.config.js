e2723b4eaf9c8b5da9136d8e9e97722af4255672
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-06-20 16:57:36 -0600

    Removed unused code from webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -15,23 +15,3 @@ module: {
       ]
     }
 }
-
-
-// const path = require('path');
-//
-// const config = {
-//   entry: './js/index.js',
-//   output: {
-//     path: path.resolve(__dirname, 'public/dist'),
-//     filename: 'bundle.js'
-//   },
-//   module: {
-//     rules: [
-//       {test: /\.(js|jsx)$/, use: 'babel-loader'},
-//       {test: /\.css$/, use: 'css-loader'},
-//       {exclude: /node_modules/}
-//     ]
-//   }
-// };
-//
-// module.exports = config;
